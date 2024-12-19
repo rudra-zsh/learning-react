62b27ecbb83d6df2521f86e7342fff0fe472f94f
rudra-zsh <rudra01@duck.com>
2024-12-19 19:53:32 +0530

    second day

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import Name from './Name.jsx'
 import {React} from 'react'
-
+import ReactDOM from 'react-dom/client';
 /*
 var Name = createRoot(document.getElementById('name')
 	Name.render(
@@ -15,7 +15,7 @@ var Name = createRoot(document.getElementById('name')
 */
 var user = 'robot'
 function Phul(){
-return (	<h1>Hello {user}</h1>
+return (	<a href="https://google.com"><h1>Hello {user}</h1></a>
 )
 }
 /*
@@ -27,14 +27,28 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	reactElement
 )
 */
+const reactElement = (
+	<a href="https://google.com">Google</a>
+)
 
+const reactProperElement = React.createElement(
+	'a',
+	{href:"https://google.com" , target:'balnk'},
+	'Google'
+)
+/*
 var rb = createRoot(document.getElementById('root'))
 	rb.render(
-
 	<StrictMode>
 	<Phul />
-		reactElement
+reactProperElement
 		<App />
 	</StrictMode>
 
+
+)
+*/
+ReactDOM.createRoot(document.getElementById('root')).render
+(
+	<App />
 )
