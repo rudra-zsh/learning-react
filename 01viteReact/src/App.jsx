d54354ce6c2db8c86b5d40f576a8c936f1a2dc17
rudra-zsh <rudra01@duck.com>
2024-12-19 20:46:41 +0530

    basic practices

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,10 +1,17 @@
 
-function App() {
 
-
-  return (
-    <h1>hello react</h1>
-  )
+function App()
+{
+return(
+	<div>
+	<h5>Hello React</h5>
+	<a href="https://google.com">This is google Link</a>
+	<p>This is a 
+	paragraph.
+	</p>
+	
+	</div>
+)
 }
 
 export default App
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -3,33 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Name from './Name.jsx'
-/*
-var Name = createRoot(document.getElementById('name')
-	Name.render(
-		<React.StrictMode>
-		<Name />
-		</React.StrictMode>
-	)
-*/
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <Name />
-	<App />
-  </StrictMode>,
-)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-	<Name />
-	<App />
-	</React.StrictMode>,
-)
+const element = createRoot(document.getElementById('root'))
 
-var root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
-	<React.StrictMode>
-<Name />
+element.render(
+	<StrictMode>
 	<App />
-	</React.StrictMode>,
+	</StrictMode>,
+
 )
