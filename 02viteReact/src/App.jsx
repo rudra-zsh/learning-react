0ffd2e02d9cc24c246c0107808e2cc814e315efd
rudra-zsh <rudra01@duck.com>
2025-01-07 11:07:50 +0530

    Add User

--- 02viteReact/src/App.jsx
+++ 02viteReact/src/App.jsx
@@ -2,21 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import User from './User.jsx'
 
-function Button(){
-  return(
+function Button() {
+  return (
     <button>Click</button>
   )
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1>Welcome to react app</h1>
-    <Button />
-      </>
+      <h1>Welcome to react app</h1>
+      <Button />
+      <User />
+    </>
   )
 }
 
